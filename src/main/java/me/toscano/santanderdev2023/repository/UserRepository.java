93d89cef8770dfcbb991db4870e3e3111d9afe4b
Gabriel Toscano <gabrieltoscano1@outlook.com>
2023-10-17 19:15:28 -0300

    Criado a camada de Repository com a interface UserRepository e criado o arquivo application.yml

---
+++ src/main/java/me/toscano/santanderdev2023/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package me.toscano.santanderdev2023.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.toscano.santanderdev2023.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
